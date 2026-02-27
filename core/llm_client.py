f58dde0a89fbee244eeec11c4dd9af5f2f49c536
sarase975-star <sarase975@gmail.com>
2026-02-27 09:00:18 +0100

    Update llm_client.py

--- core/llm_client.py
+++ core/llm_client.py
@@ -1,1 +1,17 @@
+# core/llm_client.py
 
+from openai import OpenAI
+import os
+
+client = OpenAI(
+    base_url="https://router.huggingface.co/v1",
+    api_key=os.getenv("HF_API_KEY")
+)
+
+def call_model(model, messages):
+    response = client.chat.completions.create(
+        model=model,
+        messages=messages,
+        max_tokens=1500,
+    )
+    return response.choices[0].message.content
